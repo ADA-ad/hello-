b8aa7aadc026f2cdbc0d34053e8fc0391728058f
ADA <tracyshi911@gmail.com>
2023-12-27 15:53:36 +0900

    add subブランチを作成し、 @DeleteMappingと/hello2を作成する

--- src/main/java/com/ada/hello/HelloController.java
+++ src/main/java/com/ada/hello/HelloController.java
@@ -1,5 +1,6 @@
 package com.ada.hello;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +9,15 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello() {
         return "hello world";
+
+    }
+    @DeleteMapping("/hello")
+    public  String deleteHello() {
+        return "delete hello";
+    }
+    @GetMapping("/hello2")
+    public String hello2() {
+        return "hello ADA2";
+
     }
 }
